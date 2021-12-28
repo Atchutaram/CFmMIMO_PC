5a338a4599ae80ae9c5bd13f7461f21298fe3a57
kochark1 <kameswara.kocharlakota@aalto.fi>
2021-12-28 14:21:41 +0200

    msl

--- schedulers/schedule_datagen.sh
+++ schedulers/schedule_datagen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=00:30:00
-#SBATCH --mem-per-node=1M
+#SBATCH --mem=1M
 #SBATCH --job-name=schedule-datagen
 #SBATCH --output=script_logs/schedule_datagen_%a.out
 #SBATCH --array=0-4
@@ -8,4 +8,4 @@
 sim_filename='data_logs_training/params/sim_params_1.pkl'  # do not change this; it is same as given in set_sim_params.py
 sample_id=$SLURM_ARRAY_TASK_ID
 
-srun python ./schedule_datagen.py sim_filename sample_id
+srun python ./schedulers/schedule_datagen.py sim_filename sample_id
