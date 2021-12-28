76f6679b8c71336b71745c0f7eab810a57581e95
kochark1 <kameswara.kocharlakota@aalto.fi>
2021-12-28 14:30:41 +0200

    trail

--- schedulers/schedule_datagen.py
+++ schedulers/schedule_datagen.py
@@ -1,12 +1,10 @@
 import sys
 
-from .setup_sim_params import get_sim_params
 from channel_env.generate_beta import data_gen
 
 
-
 if __name__ == '__main__':
-    
+    from setup_sim_params import get_sim_params
     argv = sys.argv[1:]
     if not argv:
         print("Something went wrong!")
@@ -18,6 +16,8 @@ if __name__ == '__main__':
     data_gen(simulation_parameters, sample_id)
 else:
     import os
+
+    from schedulers.setup_sim_params import get_sim_params
     sim_filename = os.path.join(os.getcwd(), 'data_logs_training', 'params', 'sim_params_1.pkl')
     simulation_parameters = get_sim_params(sim_filename)
     for sample_id in range(4):
