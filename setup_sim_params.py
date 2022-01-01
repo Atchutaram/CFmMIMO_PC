1f5f2c5e922a906296a192debee96c38c74c17c9
kochark1 <kameswara.kocharlakota@aalto.fi>
2022-01-01 20:14:54 +0200

    msl

--- setup_sim_params.py
+++ setup_sim_params.py
@@ -23,17 +23,13 @@ def setup_sim(argv, triton = True):
         root = os.getcwd()
     
     
-    for mode in OperatingModes:
-        print('Trace 2: ', mode)
-
     simulation_parameters = None
     if argv:
         number_of_samples, operation_mode, scenario = argv
-        print('Trace 3: operation_mode is ', operation_mode)
 
         filename = f'sim_params_{scenario}.pkl'
-        for mode in OperatingModes:
-            if mode == operation_mode:
+        for id, mode in enumerate(OperatingModes, 1):
+            if id == operation_mode:
                 operation_mode = mode
         simulation_parameters = SimulationParameters(root, number_of_samples, operation_mode, scenario)
     else:
