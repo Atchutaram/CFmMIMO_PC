20af769582546eaa0aff7cb798617cb542656295
Andrew <->
2023-03-03 16:59:33 +0200

    double heatmap plot

--- Betas_reading.py
+++ Betas_reading.py
@@ -12,8 +12,10 @@ import matplotlib.pyplot as plt
 
 # temporary hardcoded
 cwd = os.getcwd() + "\simID_2\data_logs_training\\betas"
-max_index = 100
-matrix_sum = 0
+max_index = 8000
+half_index = int(max_index / 2) 
+matrix_sum1 = 0
+matrix_sum2 = 0
 betas = []
 
 # read all betas to the list
@@ -21,14 +23,23 @@ for f_index in range(max_index):
     f_name = f'betas_sample{f_index}.pt'
     beta_file_path = os.path.join(cwd, f_name)
     m = torch.load(beta_file_path)
-    betas.append(m['betas'].numpy())
+    betas.append(np.log(m['betas'].numpy()))
+    # betas.append(m['betas'].numpy())
+
+print(betas[0].shape)
 
 # calculate correlation matrix
-for i in range(max_index):
-    matrix_sum += betas[i] @ betas[i].T
+for i in range(half_index):
+    matrix_sum1 += betas[i] @ betas[i].T
     
-corr_matrix = matrix_sum / max_index
+for i in range(half_index,max_index):
+    matrix_sum2 += betas[i] @ betas[i].T
+    # matrix_sum += betas[i].T @ betas[i]
+    
+corr_matrix = matrix_sum1 / half_index
+corr_matrix2 = matrix_sum2 / half_index
 
-plt.figure()
-sns.heatmap(corr_matrix)    
+fig, (ax1, ax2) = plt.subplots(1,2)
+sns.heatmap(corr_matrix, ax=ax1)
+sns.heatmap(corr_matrix2, ax=ax2)
 plt.show()
